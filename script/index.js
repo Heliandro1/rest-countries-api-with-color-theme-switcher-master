31d97e37b3de191a6f71efa63641317ce0b43aea
ACC <aderitocaxala10@gmail.com>
2022-12-27 15:23:32 +0100

    adding the api

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 const btnFilter = document.querySelector("#btnFilter");
 btnFilter.addEventListener("click", showDropDown);
-
+window.addEventListener("load", getAllCountriesInfo);
 function showDropDown() {
     const navOpt = document.querySelector("div.filter-container > nav");
     if (navOpt.style.display == '') {
@@ -12,7 +12,39 @@ function showDropDown() {
             navOpt.style.display = '';
         }, 1000);
        
-    }
-    
-    
+    }   
+}
+function createCountryCard(name, population, region, capital, img) {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    const divImg = document.createElement("div");
+    divImg.classList.add("country-flag-img");
+    divImg.style.backgroundImage = `url(${img})`;
+    div.append(divImg);
+    div.innerHTML += `
+    <div class="card-details">
+        <p id="countryName"><strong>${name}</strong></p>
+        <div class="details">
+            <p>Population: <span>${population}</span></p>
+            <p>Region: <span>${region}</span></p>
+            <p>Capital: <span>${capital}</span></p>
+        </div>
+    </div>`;
+    return div;
+}
+function addCards(callback, ...data) {
+    const sect = document.querySelector("section.countries-cards");
+    data.forEach(element => {
+        element.forEach(countrie => {
+            sect.append(callback(countrie.name.official, countrie.population, countrie.region, countrie.capital, countrie.flags.svg));
+        });
+       
+    });
+}
+async function getAllCountriesInfo() {
+    const res = await fetch("https://restcountries.com/v3.1/all");
+    const data = await res.json();
+    addCards(createCountryCard, data);
+    console.log(data);
+    return data;
 }
